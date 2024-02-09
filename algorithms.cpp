7ad0c604fde0f87ac1e04d84f7e5ddecff25f368
Ben Cuff <156725385+ben-cuff@users.noreply.github.com>
2024-02-09 19:50:32 +0000

    updated algorithms.cpp

--- algorithms.cpp
+++ algorithms.cpp
@@ -2,12 +2,17 @@
 #include <cstdlib>
 #include <ctime>
 #include <chrono>
+#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
 const int arraySize = 100000;
 
-void linear_Search(int *, int);
+void linear_Search(int *arr, int valueToFind);
+void binary_Search(int *arr, int valueToFind);
+void ternary_Search(int *arr, int valueToFind);
+void jump_Search(int *arr, int valueToFind);
 
 int main()
 {
@@ -20,7 +25,7 @@ int main()
         arr[i] = rand() % 1000000 + 1; // Generate a random number between 1 and 1000000
     }
 
-    int indexToFind = rand() % arraySize + 1;
+    int indexToFind = rand() % arraySize;
 
     auto start = chrono::high_resolution_clock::now();
 
@@ -28,9 +33,39 @@ int main()
 
     auto end = chrono::high_resolution_clock::now();
 
+    cout << "Linear Search took " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << " nanoseconds to run" << endl;
+
+    int copyArr[arraySize];
+    for (int i = 0; i < arraySize; ++i)
+    {
+        copyArr[i] = arr[i];
+    }
+
+    sort(copyArr, copyArr + arraySize);
+
+    start = chrono::high_resolution_clock::now();
+
+    binary_Search(copyArr, arr[indexToFind]);
+
+    end = chrono::high_resolution_clock::now();
+
     cout << "Binary Search took " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << " nanoseconds to run" << endl;
 
     start = chrono::high_resolution_clock::now();
+
+    ternary_Search(copyArr, arr[indexToFind]);
+
+    end = chrono::high_resolution_clock::now();
+
+    cout << "Ternary Search took " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << " nanoseconds to run" << endl;
+
+    start = chrono::high_resolution_clock::now();
+
+    jump_Search(copyArr, arr[indexToFind]);
+
+    end = chrono::high_resolution_clock::now();
+
+    cout << "Jump Search took " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << " nanoseconds to run" << endl;
 
     return 0;
 }
@@ -47,18 +82,82 @@ void linear_Search(int *arr, int valueToFind)
     }
 }
 
-void binarySearch(int arr[], int valueToFind)
+void binary_Search(int *arr, int valueToFind)
 {
-    int l = 0, r = arraySize;
+    int l = 0, r = arraySize - 1;
     while (l <= r)
     {
         int m = l + (r - l) / 2;
         if (arr[m] == valueToFind)
+        {
             cout << "Binary Search: Found " << valueToFind << " at index " << m << endl;
             return;
+        }
         if (arr[m] < valueToFind)
             l = m + 1;
         else
             r = m - 1;
     }
+    cout << "not found" << endl;
+}
+
+void ternary_Search(int *arr, int valueToFind)
+{
+    int l = 0, r = arraySize - 1;
+    while (l <= r)
+    {
+        int mid1 = l + (r - l) / 3;
+        int mid2 = r - (r - l) / 3;
+
+        if (arr[mid1] == valueToFind)
+        {
+            cout << "Ternary Search: Found " << valueToFind << " at index " << mid1 << endl;
+            return;
+        }
+
+        if (arr[mid2] == valueToFind)
+        {
+            cout << "Ternary Search: Found " << valueToFind << " at index " << mid2 << endl;
+            return;
+        }
+
+        if (valueToFind < arr[mid1])
+            r = mid1 - 1;
+        else if (valueToFind > arr[mid2])
+            l = mid2 + 1;
+        else
+        {
+            l = mid1 + 1;
+            r = mid2 - 1;
+        }
+    }
+    cout << "not found" << endl;
+}
+
+void jump_Search(int *arr, int valueToFind)
+{
+    int step = sqrt(arraySize);
+    int prev = 0;
+
+    while (arr[min(step, arraySize) - 1] < valueToFind)
+    {
+        prev = step;
+        step += sqrt(arraySize);
+        if (prev >= arraySize)
+        {
+            cout << "not found" << endl;
+            return;
+        }
+    }
+
+    for (int i = prev; i < min(step, arraySize); ++i)
+    {
+        if (arr[i] == valueToFind)
+        {
+            cout << "Jump Search: Found " << valueToFind << " at index " << i << endl;
+            return;
+        }
+    }
+
+    cout << "not found" << endl;
 }
