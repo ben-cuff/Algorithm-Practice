9a1749afeed963f6bce9e4238f0dec615e27ded3
Ben Cuff <156725385+ben-cuff@users.noreply.github.com>
2024-02-09 19:57:08 +0000

    more updates

--- algorithms.cpp
+++ algorithms.cpp
@@ -13,6 +13,8 @@ void linear_Search(int *arr, int valueToFind);
 void binary_Search(int *arr, int valueToFind);
 void ternary_Search(int *arr, int valueToFind);
 void jump_Search(int *arr, int valueToFind);
+void interpolation_Search(int *arr, int valueToFind);
+int fibonacci_Search(int *arr, int valueToFind);
 
 int main()
 {
@@ -66,6 +68,22 @@ int main()
     end = chrono::high_resolution_clock::now();
 
     cout << "Jump Search took " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << " nanoseconds to run" << endl;
+
+    start = chrono::high_resolution_clock::now();
+
+    interpolation_Search(copyArr, arr[indexToFind]);
+
+    end = chrono::high_resolution_clock::now();
+
+    cout << "Interpolation Search took " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << " nanoseconds to run" << endl;
+
+    start = chrono::high_resolution_clock::now();
+
+    int result = fibonacci_Search(copyArr, arr[indexToFind]);
+
+    end = chrono::high_resolution_clock::now();
+
+    cout << "Fibonacci Search took " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << " nanoseconds to run" << endl;
 
     return 0;
 }
@@ -160,4 +178,81 @@ void jump_Search(int *arr, int valueToFind)
     }
 
     cout << "not found" << endl;
+}
+
+void interpolation_Search(int *arr, int valueToFind)
+{
+    int l = 0, r = arraySize - 1;
+
+    while (l <= r && arr[l] <= valueToFind && arr[r] >= valueToFind)
+    {
+        // Use interpolation formula to find the probable position
+        int pos = l + ((valueToFind - arr[l]) * (r - l)) / (arr[r] - arr[l]);
+
+        if (arr[pos] == valueToFind)
+        {
+            cout << "Interpolation Search: Found " << valueToFind << " at index " << pos << endl;
+            return;
+        }
+
+        if (arr[pos] < valueToFind)
+            l = pos + 1;
+        else
+            r = pos - 1;
+    }
+
+    cout << "not found" << endl;
+}
+
+int fibonacci_Search(int *arr, int valueToFind)
+{
+    int fibM_minus_2 = 0;                   // (m-2)'th Fibonacci number
+    int fibM_minus_1 = 1;                   // (m-1)'th Fibonacci number
+    int fibM = fibM_minus_1 + fibM_minus_2; // m'th Fibonacci number
+
+    // Find the smallest Fibonacci number greater than or equal to array size
+    while (fibM < arraySize)
+    {
+        fibM_minus_2 = fibM_minus_1;
+        fibM_minus_1 = fibM;
+        fibM = fibM_minus_1 + fibM_minus_2;
+    }
+
+    int offset = -1; // Offset to start comparing from
+
+    while (fibM > 1)
+    {
+        // Check if fibM_minus_2 is a valid index
+        int i = min(offset + fibM_minus_2, arraySize - 1);
+
+        // If the element is greater than the target value, move fibM positions back
+        if (arr[i] > valueToFind)
+        {
+            fibM = fibM_minus_2;
+            fibM_minus_1 = fibM_minus_1 - fibM_minus_2;
+            fibM_minus_2 = fibM - fibM_minus_1;
+        }
+        // If the element is smaller than the target value, move fibM positions ahead
+        else if (arr[i] < valueToFind)
+        {
+            fibM = fibM_minus_1;
+            fibM_minus_1 = fibM_minus_2;
+            fibM_minus_2 = fibM - fibM_minus_1;
+            offset = i;
+        }
+        // Element found
+        else
+        {
+            return i;
+        }
+    }
+
+    // Compare the last element
+    if (fibM_minus_1 && arr[offset + 1] == valueToFind)
+    {
+        return offset + 1;
+    }
+
+    // If not found, return -1
+    return -1;
 }
